961f1f110a4cc61fb049899b3cd7d1209812395c
ljiang09 <ljiang@olin.edu>
2024-12-14 16:26:33 -0500

    example for grabbing API info

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           TURING MACHINE HERE
         </p>
+        <p>The current time is {currentTime}.</p>
       </header>
     </div>
   );
