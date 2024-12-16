36af6f3652a750a6a09c871f046a4eefe48f8045
ljiang09 <ljiang@olin.edu>
2024-12-16 17:29:36 -0500

    add visual representation of TM steps

--- src/components/Output.js
+++ src/components/Output.js
@@ -2,6 +2,29 @@
 
 function Output({outputStr}) {
 
+  const parseString = (str) => {
+    return str.map((line) => {
+      
+      let modifiedLine = line.replace(/ /g, '');
+      let newLine = []
+
+      for (let index = 0; index < modifiedLine.length; index++) {
+        let char = modifiedLine[index];
+        if (char === "[" || char === "]") continue;
+        else {
+          const isInBrackets = modifiedLine[index - 1] === "[" && modifiedLine[index + 1] === "]";
+          const content = isInBrackets ? (
+            <span key={index} className="bracketed">{char}</span>
+          ) : (
+            <span key={index} className="normal">{char}</span>
+          );
+          newLine.push(content);
+        }
+      }
+      return newLine;
+    });
+  };
+
   return (
     <div
       style={{ 
@@ -9,9 +32,11 @@ function Output({outputStr}) {
         justifyContent: 'center'
       }}
     >
-      <div style={{ textAlign: 'left' }}>
-        {outputStr.map((line, index) => (
-          <p key={index}>{line}</p>
+      <div style={{ textAlign: 'left', display: 'flex', flexDirection: "column", gap: "3px" }}>
+        {parseString(outputStr).map((line, idx) => (
+          <div key={idx} style={{display: 'flex', gap: "1px"}}>
+            {line}
+          </div>
         ))}
       </div>
       {/* TODO: convert string output of turing machine into nice boxes */}
