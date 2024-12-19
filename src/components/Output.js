43ba97bab4ea51997047ed4a98f7789751009d9b
ljiang09 <ljiang@olin.edu>
2024-12-19 15:33:13 -0500

    add animated frames to output

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,7 +1,7 @@
-// import {TextField} from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import {ToggleButtonGroup, ToggleButton} from '@mui/material';
 
 function Output({outputStr}) {
-
   const parseString = (str) => {
     return str.map((line) => {
       
@@ -25,21 +25,79 @@ function Output({outputStr}) {
     });
   };
 
+  var parsedString = parseString(outputStr);
+  const [displayType, setDisplayType] = useState(true);
+  const [frame, setFrame] = useState(0);
+
+  useEffect(() => {
+    if (displayType === false) {
+      const interval = setInterval(() => {
+        setFrame((prev) => {
+          if (prev >= parsedString.length - 1) {
+            prev = 0
+            return prev
+          }
+          return prev + 1;
+        });
+      }, 1000);
+
+      return () => clearInterval(interval);
+    } else {
+      setFrame(0);
+    }
+  }, [displayType]);
+
   return (
     <div
       style={{ 
         display: 'flex', 
-        justifyContent: 'center'
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
       }}
     >
-      <div style={{ textAlign: 'left', display: 'flex', flexDirection: "column", gap: "3px" }}>
-        {parseString(outputStr).map((line, idx) => (
-          <div key={idx} style={{display: 'flex', gap: "1px"}}>
-            {line}
+      <ToggleButtonGroup
+        value={displayType}
+        exclusive
+        onChange={() => setDisplayType(!displayType)}
+        style={{paddingBottom: "20px"}}
+      >
+        <ToggleButton value={true} style={{ height: "50px", textTransform: "none" }}>
+          <p>Animated</p>
+        </ToggleButton>
+        <ToggleButton value={false} style={{ height: "50px", textTransform: "none" }}>
+          <p>Step-by-step</p>
+        </ToggleButton>
+      </ToggleButtonGroup>
+      {displayType ? 
+        <div style={{ textAlign: 'left', display: 'flex', flexDirection: "column", gap: "3px" }}>
+          {parsedString.map((line, idx) => (
+            <div key={idx} style={{display: 'flex', gap: "1px"}}>
+              {/* {idx} */} {line}
+            </div>
+          ))}
+        </div>
+        :
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: "column",
+            justifyContent: "center",
+            gap: "3px"
+          }}
+        >
+          <div>{frame}</div>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: "center",
+              gap: "1px"
+            }}
+          >
+            {parsedString[frame]}
           </div>
-        ))}
-      </div>
-      {/* TODO: convert string output of turing machine into nice boxes */}
+        </div>
+      }
     </div>
   );
 }
